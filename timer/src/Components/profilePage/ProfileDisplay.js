1f611c04263866af6177b9f824c8ca4d52248617
Emily Zou <emily@d-172-25-230-219.dhcp.virginia.edu>
2019-05-29 13:08:48 -0400

    update

--- timer/src/Components/profilePage/ProfileDisplay.js
+++ timer/src/Components/profilePage/ProfileDisplay.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 export default class ProfileDisplay extends React.Component {
+    
+    state = {
+        tasks: []
+    }
 
     render () {
         return (
@@ -10,6 +14,12 @@ export default class ProfileDisplay extends React.Component {
                      <td>Date</td>
                      <td>Time</td>
                      <td>Task</td>
+                 </tr>
+                 {/* Map data from firebase to here for the user */}
+                 <tr>
+                     <td>June 21st, 2019</td>
+                     <td>10:00am</td>
+                     <td>Build website</td>
                  </tr>
              </table>
         </div>
